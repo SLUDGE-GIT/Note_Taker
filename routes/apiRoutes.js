87fb771a0a3197a3b2e8485e6119522bd68668ae
Kyle <florez.photo@gmail.com>
2021-05-05 12:11:57 -0500

    finally figured out my problem - everything fully works (except delete)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 // var uniqid = require('uniqid');
-const { v4: uuidv4 } = require('uuid');
 // import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const storedNotes = require('../db/db.json');
+var express = require('express');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(storedNotes));
 
     app.post('/api/notes', (req, res) => {
-        let addNote = req.body;
+        const addNote = req.body;
         addNote.id = uuidv4();
         
         storedNotes.push(addNote);
-        fs.writeFile(path.resolve(__dirname, '../db/db.json'), JSON.stringify(addNote), err => {
-            err ? console.log(err) : console.log ('Note Succefully Saved');
-            res.json("../db/db.json");
-
+        fs.writeFile(__dirname + '/../db/db.json', JSON.stringify(storedNotes), (err) => {
+            err ? console.log(err) : console.log('Note Successfully Saved');
+            
+   
         });
-    });
-};
+        res.json('../db/db.json');
+    })
+}
 
